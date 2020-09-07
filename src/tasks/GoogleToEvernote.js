20b6fbe6e8c50510f11fb148a5223067eaca08e4
Joe Gatt <joe@joegatt.net>
2020-09-07 10:44:07 +0200

    Add auth pages

--- src/tasks/GoogleToEvernote.js
+++ src/tasks/GoogleToEvernote.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/documents.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/documents'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -108,7 +108,7 @@ const formatBody = data => {
     if (chunk.paragraph.paragraphStyle.namedStyleType === 'HEADING_5') {
       const headingText = chunk.paragraph.elements[0].textRun.content.trim();
       if (headingText === '--30--' || headingText === '-30-') return headingText;
-      return [' ',`{\{${headingText}\}\}`.replace(/\{\{\{\{/, '{{').replace(/\}\}\}\}/, '}}')];
+      return [' ',`{\{${headingText}\}\}`];
     }
     return chunk.paragraph.elements.map(element => {
       if (element.textRun && element.textRun.textStyle.link) {
@@ -125,25 +125,93 @@ const formatBody = data => {
   return textString;
 }
 
-const chapters = [
+const formatBodyFromGoogleDoc = data => {
+  // This is probably temporary since we will want the permanent one to do this conversion from Evernote format
+  const textArray = data.body.content.map(chunk => {
+    if (!chunk.paragraph) return null;
+    if (chunk.paragraph.paragraphStyle.namedStyleType === 'TITLE') return null;
+    if (chunk.paragraph.paragraphStyle.namedStyleType === 'HEADING_4') return null;
+    if (chunk.paragraph.paragraphStyle.namedStyleType === 'HEADING_5') return '\n';
+    return chunk.paragraph.elements.map(element => {
+      if (element.textRun && element.textRun.textStyle.link) {
+        return `<a href="${element.textRun.textStyle.link.url}">${element.textRun.content.trim()}</a>`;
+      }
+      if (element.textRun) return element.textRun.content.trim();
+      if (element.footnoteReference) return null;
+      return null;
+    }).join(' ');
+  });
+  const textString = textArray.flat().filter(Boolean).map(line => line && `${line.replace(/\&/gm, '&amp;').replace(/\n\n\n+/gm, '\n\n').replace(/ +/gm, ' ')}\n`).join('').trim();
+  return textString.replace(/\n\n\n\t/gm, '\n\n\n');
+}
+
+let chapters = [
   {
-    name: 'Example',
+    evernoteId: '4d6bf3b8-0c94-44f1-a1fb-c0e37faf4213',
     googleDocumentId: '1mhAQIzBflcx_jxIejYOrfWWgEnjCD6Kpa9eTxFTFdF0',
-    evernoteId: '4d6bf3b8-0c94-44f1-a1fb-c0e37faf4213'
+    googleDocumentIdNoAnnotations: '1HIOXwiF1Zm2-PpUrYlvM_S7NjUjA45IgxHaf3KkeC_k',
+    name: 'Example',
   },
-  {}, {}, {},
   {
-    name: 'Chapter 4',
+    googleDocumentId: '1Hw83EEy5rPZXhpWHAvZTS4rPOU7Evyyr1lWwOHB_Y2M',
+    googleDocumentIdNoAnnotations: '1HIOXwiF1Zm2-PpUrYlvM_S7NjUjA45IgxHaf3KkeC_k',
+    evernoteId: 'bdaab182-b0fd-4341-b14c-caccf1398e75',
+    name: 'Chapter 1',
+  }, 
+  {
+    googleDocumentId: '17dBUrOnwO-4c6QQM9UyfiUI1CBfV0jPPB1aQn9sbMt4',
+    googleDocumentIdNoAnnotations: '1HIOXwiF1Zm2-PpUrYlvM_S7NjUjA45IgxHaf3KkeC_k',
+    evernoteId: 'b294b6a5-a561-465a-85b7-8a692b21225e',
+    name: 'Chapter 2',
+  },
+  {
+    googleDocumentId: '1j0j8EHzrk06cadp56yyLgBJ1gbJ54_DWPdcVJV2GWi8',
+    googleDocumentIdNoAnnotations: '1HIOXwiF1Zm2-PpUrYlvM_S7NjUjA45IgxHaf3KkeC_k',
+    evernoteId: '4d7f9c6d-287a-4847-9cfd-1b4fda7c3a41',
+    name: 'Chapter 3',
+  },
+  {
+    evernoteId: '3c596fe9-d166-4c69-bc9a-ca5ec1cb9888',
     googleDocumentId: '13LhjdMQiQvQJfqy9EhcIICp5Zmp06hnjqRoDCC1shNI',
-    evernoteId: '3c596fe9-d166-4c69-bc9a-ca5ec1cb9888'
+    googleDocumentIdNoAnnotations: '1HIOXwiF1Zm2-PpUrYlvM_S7NjUjA45IgxHaf3KkeC_k',
+    name: 'Chapter 4',
   },
   {
-    name: 'Chapter 5',
     googleDocumentId: '1BsLoH3GnAWUMss04IcTxPQZkbv0suV3zuYg5r7ZHXgY',
-    evernoteId: 'c967e410-ebdb-4de8-aade-9f7b683e83e7'
+    googleDocumentIdNoAnnotations: '1HIOXwiF1Zm2-PpUrYlvM_S7NjUjA45IgxHaf3KkeC_k',
+    evernoteId: 'c967e410-ebdb-4de8-aade-9f7b683e83e7',
+    name: 'Chapter 5'
   },
   { name: 'Chapter 6', googleDocumentId:'', evernoteId: '0a56703b-f884-4a13-901d-a4cb81d55c90' }
 ];
+
+chapters[100] = {
+  googleDocumentId: '1y49ohNV8tMnHl07Esuw2kgiCXqidmXbTbe3sGGqwSmg',
+  googleDocumentIdNoAnnotations: '1FKZIfZabCPdksBFFVhxS9M1CRBXf44c57YkCS7-ezh8',
+  evernoteId: '18b66cf9-93ac-4fde-91cc-93e1571e27fe',
+  name: 'Qatel 0'
+};
+
+chapters[101] = {
+  googleDocumentId: '1cPOzhm-0FfryD5uJQ_RBMYNIKr7QYVpj-6K28I6WCHE',
+  googleDocumentIdNoAnnotations: '1FKZIfZabCPdksBFFVhxS9M1CRBXf44c57YkCS7-ezh8',
+  evernoteId: 'c967e410-ebdb-4de8-aade-9f7b683e83e7',
+  name: 'Qatel 1'
+};
+
+chapters[102] = {
+  googleDocumentId: '1JYDHsmJyIZ-WXj7xceP0ZimBJ62s4KHHz_Ed44SQCYE',
+  googleDocumentIdNoAnnotations: '1FKZIfZabCPdksBFFVhxS9M1CRBXf44c57YkCS7-ezh8',
+  evernoteId: 'c967e410-ebdb-4de8-aade-9f7b683e83e7',
+  name: 'Qatel 2'
+};
+
+chapters[1001] = {
+  googleDocumentId: '1BsLoH3GnAWUMss04IcTxPQZkbv0suV3zuYg5r7ZHXgY',
+  googleDocumentIdNoAnnotations: '1FKZIfZabCPdksBFFVhxS9M1CRBXf44c57YkCS7-ezh8',
+  evernoteId: 'c967e410-ebdb-4de8-aade-9f7b683e83e7',
+  name: 'Political 1'
+};
 
 /**
  * Prints the title of a sample doc:
@@ -208,6 +276,22 @@ const chapters = [
     if (err) return console.log('The API returned an error: ' + err);
     const documentTitle = res.data.title;
     const bodyText = formatBody(res.data);
+
+    docs.documents.batchUpdate({
+      documentId: text.googleDocumentIdNoAnnotations,
+      requestBody: {
+        requests: [
+          {
+            insertText: {
+              location: {
+                index: 1
+              },
+              text: formatBodyFromGoogleDoc(res.data)
+            }
+          }
+        ]
+      }
+    });
 
     const contentHash = md5(`${documentTitle}${bodyText}`)
     const fileName = `${parameterize(documentTitle)}|${text.evernoteId}|${Date.now()}|${contentHash}.txt`;
