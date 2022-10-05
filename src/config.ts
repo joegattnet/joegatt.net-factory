69ab7db8d6c0389430db2fa473dec98a4414cc08
Joe Gatt <joe@joegatt.net>
2022-10-05 21:58:08 +0200

    Revert

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ module.exports = {
   BLURB_LENGTH: 50,
   DB_CONNECTION: {
     user: "deployer",
-    host: "postgres",
+    host: "172.20.0.2",
     database: "joegattnet", // THESE SHOULDN'T BE HARDCODED!
     password: "itTieni10",
     port: 5432,
